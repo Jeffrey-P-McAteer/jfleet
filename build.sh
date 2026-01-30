b06a84fd93cd0a709fa0abaeb001dbc889170a94
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-01-30 12:35:54 -0500

    clean-up

--- build.sh
+++ build.sh
@@ -2,24 +2,43 @@
 
 VM_IMAGE="out/jfleet-node.qcow2"
 
-mkdir -p $(dirname "$VM_IMAGE")
+set -e
+
+OUT_DIR=$(dirname "$VM_IMAGE")
+
+mkdir -p "$OUT_DIR"
+mkdir -p "$OUT_DIR"/completed
 
 if ! [[ -e "$VM_IMAGE" ]] ; then
   virt-builder centosstream-9 -o "$VM_IMAGE" --size 20G
 fi
 
+customize_step() {
+  STEP_NAME="$1"
+  FLAG_FILE="$OUT_DIR/completed/$STEP_NAME"
+  if [[ -e "$FLAG_FILE" ]] ; then
+    echo "Step $STEP_NAME completed, skipping."
+  else
+    virt-customize -a "$VM_IMAGE" "${@:2}"
+    touch "$FLAG_FILE"
+  fi
+}
 
-virt-customize -a "$VM_IMAGE" \
-  --install vim,git,bash-completion,python \
+customize_step install-packages --install vim,git,bash-completion,python
+
+customize_step create-user \
   --run-command 'useradd -m -G wheel -s /bin/bash user' \
   --run-command 'mkdir -p /etc/systemd/system/getty@tty1.service.d/' \
   --copy-in login-controls/autologin.conf:'/etc/systemd/system/getty@tty1.service.d/' \
   --run-command 'mkdir -p /etc/sudoers.d/' \
-  --copy-in login-controls/user:/etc/sudoers.d/ \
+  --copy-in login-controls/user:/etc/sudoers.d/
+
+customize_step setup-pycomms \
   --run-command 'mkdir -p /opt/pycomms/' \
   --copy-in pycomms/pycomms_server.py:/opt/pycomms/ \
   --copy-in pycomms/pycomms-server.service:/etc/systemd/system/ \
   --run-command 'systemctl enable pycomms-server.service' \
-  --run-command 'echo done'
 
+
+virt-customize -a "$VM_IMAGE" --run-command 'echo my hostname is $(hostname)'
 
--- readme.md
+++ readme.md
@@ -5,35 +5,17 @@
 yay -S guestfs-tools
 
 virt-builder --list
-
-# # "Server" - knows about build stages and transforms toml => json => images
-# yay -S osbuild-composer
-
-# sudo systemctl enable --now osbuild-composer.socket
-# sudo systemctl enable --now osbuild-composer.service
-
-# # Client
-
-# go install github.com/osbuild/image-builder-cli/cmd/image-builder@main
-
-# image-builder list
-# image-builder list --filter 'arch:x86_64' --filter 'type:minimal-raw'
-# image-builder list --filter 'arch:x86_64' --filter 'type:qcow2'
 ```
 
 # Building the Images
 
 ```bash
-# image-builder build qcow2 --arch x86_64 --blueprint jfleet-blueprint.toml --distro rhel-10.2 --progress verbose --output-dir build
-# image-builder build img --arch x86_64 --blueprint jfleet-blueprint.toml --distro rhel-10.2 --progress verbose --output-dir build
-
-
-
+./build.sh
 ```
 
 
 # Docs
 
- - https://osbuild.org/docs/user-guide/blueprint-reference/
+ - https://libguestfs.org/virt-customize.1.html
 
 
