fd995fdacfa3850bc47b8db8858a50aa8bbe80c1
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-01-30 12:08:54 -0500

    misc++

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-VM_IMAGE="build/jfleet-node.qcow2"
+VM_IMAGE="out/jfleet-node.qcow2"
+
+mkdir -p $(dirname "$VM_IMAGE")
 
 if ! [[ -e "$VM_IMAGE" ]] ; then
   virt-builder centosstream-9 -o "$VM_IMAGE" --size 20G
@@ -8,6 +10,8 @@ fi
 
 
 virt-customize -a "$VM_IMAGE" \
+  --install vim,git,htop,tmux,bash-completion,python \
+  --run-command 'useradd -m -G wheel -s /bin/bash user' \
   --run-command 'mkdir -p /opt/pycomms/' \
   --copy-in login-controls/autologin.conf:'/etc/systemd/system/getty@tty1.service.d/autologin.conf' \
   --copy-in login-controls/user:/etc/sudoers.d/user \
